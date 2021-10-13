1886414966bebfa441ccb44955a0debce2d0f049
tun71400 <77815590+tun71400@users.noreply.github.com>
2021-10-13 15:45:57 -0400

    updated names to be correct

--- app/src/main/java/edu/temple/imageactivityproject/ImageActivity.kt
+++ app/src/main/java/edu/temple/imageactivityproject/ImageActivity.kt
@@ -14,6 +14,7 @@ class ImageActivity : AppCompatActivity() {
 
 
          val champs = resources.getStringArray(R.array.champs)
+        val champNames = resources.getStringArray(R.array.champNames)
 
          var imageAdapter: ImageAdapter = ImageAdapter(this)
          var dataList = mutableListOf<ChampData>()
@@ -21,16 +22,16 @@ class ImageActivity : AppCompatActivity() {
         champRecycler.layoutManager = GridLayoutManager(this,2)
         champRecycler.adapter = imageAdapter
 
-        dataList.add(ChampData("some",champs[0],R.drawable.ahri))
-        dataList.add(ChampData("some",champs[1],R.drawable.ashe))
-        dataList.add(ChampData("some",champs[2],R.drawable.dr_mundo))
-        dataList.add(ChampData("some",champs[3],R.drawable.garen))
-        dataList.add(ChampData("some",champs[4],R.drawable.morgana))
-        dataList.add(ChampData("some",champs[5],R.drawable.sett))
-        dataList.add(ChampData("some",champs[6],R.drawable.shen))
-        dataList.add(ChampData("some",champs[7],R.drawable.soraka))
-        dataList.add(ChampData("some",champs[8],R.drawable.viego))
-        dataList.add(ChampData("some",champs[9],R.drawable.yone))
+        dataList.add(ChampData(champNames[0],champs[0],R.drawable.ahri))
+        dataList.add(ChampData(champNames[1],champs[1],R.drawable.ashe))
+        dataList.add(ChampData(champNames[2],champs[2],R.drawable.dr_mundo))
+        dataList.add(ChampData(champNames[3],champs[3],R.drawable.garen))
+        dataList.add(ChampData(champNames[4],champs[4],R.drawable.morgana))
+        dataList.add(ChampData(champNames[5],champs[5],R.drawable.sett))
+        dataList.add(ChampData(champNames[6],champs[6],R.drawable.shen))
+        dataList.add(ChampData(champNames[7],champs[7],R.drawable.soraka))
+        dataList.add(ChampData(champNames[8],champs[8],R.drawable.viego))
+        dataList.add(ChampData(champNames[9],champs[9],R.drawable.yone))
         imageAdapter.setDataList(dataList)
 
 
